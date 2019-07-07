2de5f296249cc445ea5e19ce1f40f5bcb9bcabe6
sandeshb <sandesh.b8629@gmail.com>
2019-07-07 16:56:35 +0530

    updated files for deployment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,10 +4,10 @@
 
 export const environment = {
   production: false,
-  serverUrl: 'https://admin.tryion.shop/parse',
-  appUrl: 'https://tryion.shop',
-  appImageUrl: 'https://tryion.shop/assets/imgs/ionshop.png',
-  appId: 'JrWy7sUKLL',
+  serverUrl: 'http://13.232.63.163/parse',
+  appUrl: 'https://www.stirpot.in',
+  appImageUrl: 'https://www.stirpot.in/assets/icon/Stirpot.png',
+  appId: 'PiG5xQMoVv',
   fbId: '2081754298556977',
   stripePublicKey: 'pk_test_MVWLbqwGEHDv5J9MkdN4i7Ot',
   androidHeaderColor: '#5D67D4',
